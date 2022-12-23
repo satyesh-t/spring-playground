5b4a39dc165d81c8d204775931ceb12e28dfcd35
satyesht <satyesh.cse@gmail.com>
2022-12-23 09:15:37 -0600

    Inital Commit

---
+++ src/main/java/learn/spring/springplayground/SpringPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package learn.spring.springplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlaygroundApplication.class, args);
+	}
+
+}
